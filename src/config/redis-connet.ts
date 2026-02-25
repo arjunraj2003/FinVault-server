b9cf367828c05377d944d5403f6f8adbe0225ea3
arjunraj <arjunraj.e@reizend.ai>
2026-02-25 15:48:24 +0530

    redis

--- src/config/redis-connet.ts
+++ src/config/redis-connet.ts
@@ -1,16 +1,16 @@
-import { createClient } from "redis";
+// import { createClient } from "redis";
 
-export const redisClient = createClient({
-  url: process.env.REDIS_URL || "redis://localhost:6379",
-});
+// export const redisClient = createClient({
+//   url: process.env.REDIS_URL || "redis://localhost:6379",
+// });
 
-redisClient.on("error", (err) => {
-  console.error("Redis error:", err);
-});
+// redisClient.on("error", (err) => {
+//   console.error("Redis error:", err);
+// });
 
-export const connectRedis = async () => {
-  if (!redisClient.isOpen) {
-    await redisClient.connect();
-    console.log("Redis connected");
-  }
-};
+// export const connectRedis = async () => {
+//   if (!redisClient.isOpen) {
+//     await redisClient.connect();
+//     console.log("Redis connected");
+//   }
+// };
