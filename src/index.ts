3c1e49ad8a662727b2bfe68a88bc562199e1e61f
arjunraj <arjunraj.e@reizend.ai>
2026-03-09 17:16:21 +0530

    issues testing

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import app from "./app";
 import { AppDataSource } from "./config/data-source";
 // import { redisClient } from "./config/redis-connet";
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000; 
 
 async function startServer() {
   try {
