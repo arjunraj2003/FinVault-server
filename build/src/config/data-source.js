911b9a9dc6652018c87fe10f578f4fc30ce62fcf
arjunraj <arjunraj.e@reizend.ai>
2026-03-12 17:04:24 +0530

    build issues solved

--- build/src/config/data-source.js
+++ build/src/config/data-source.js
@@ -11,26 +11,26 @@ const Account_entity_1 = require("../entity/Account.entity");
 const Transaction_entity_1 = require("../entity/Transaction.entity");
 const Budget_entity_1 = require("../entity/Budget.entity");
 dotenv_1.default.config();
+// export const AppDataSource = new DataSource({
+//     type: "postgres",
+//     host: process.env.DB_HOST,
+//     port: parseInt(process.env.DB_PORT || "5432"),
+//     username: process.env.DB_USERNAME,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME,
+//     synchronize: true,
+//     logging: false,
+//     entities: [User,Account,Transaction,Budget],
+// });
 exports.AppDataSource = new typeorm_1.DataSource({
     type: "postgres",
-    host: process.env.DB_HOST,
-    port: parseInt(process.env.DB_PORT || "5432"),
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    synchronize: true,
+    url: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false,
+    },
+    synchronize: false,
     logging: false,
     entities: [User_entity_1.User, Account_entity_1.Account, Transaction_entity_1.Transaction, Budget_entity_1.Budget],
+    migrations: ["dist/migrations/*.js"],
 });
-// export const AppDataSource = new DataSource({
-//   type: "postgres",
-//   url: process.env.DATABASE_URL,
-//   ssl: {
-//     rejectUnauthorized: false,
-//   },
-//   synchronize: false,
-//   logging: false,
-//   entities: [User, Account, Transaction, Budget],
-//   migrations: ["dist/migrations/*.js"],
-// });
 //# sourceMappingURL=data-source.js.map
