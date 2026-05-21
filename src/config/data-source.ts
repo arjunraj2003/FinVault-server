70c0996f623f228510a6741d24527e43a4fd3909
arjunraj <arjunraj.e@reizend.ai>
2026-05-21 17:54:29 +0530

    datasource chaged

--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -12,27 +12,27 @@ dotenv.config();
 
 
 
-export const AppDataSource = new DataSource({
-    type: "postgres",
-    host: process.env.DB_HOST,
-    port: parseInt(process.env.DB_PORT || "5432"),
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    synchronize: true,
-    logging: false,
-
-    entities: [User,Account,Transaction,Budget,TransactionCategory,CreditCardDetails,CreditCardStatement],
-})
-
-
 // export const AppDataSource = new DataSource({
-//   type: "postgres",
-//   url: process.env.DATABASE_URL,
-//   ssl: {
-//     rejectUnauthorized: false,
-//   },
-//   synchronize: process.env.NODE_ENV !== "production",
-//   logging: false,
-//   entities: [User,Account,Transaction,Budget,TransactionCategory,CreditCardDetails,CreditCardStatement],
-// });
+//     type: "postgres",
+//     host: process.env.DB_HOST,
+//     port: parseInt(process.env.DB_PORT || "5432"),
+//     username: process.env.DB_USERNAME,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME,
+//     synchronize: true,
+//     logging: false,
+
+//     entities: [User,Account,Transaction,Budget,TransactionCategory,CreditCardDetails,CreditCardStatement],
+// })
+
+
+export const AppDataSource = new DataSource({
+  type: "postgres",
+  url: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+  synchronize: process.env.NODE_ENV !== "production",
+  logging: false,
+  entities: [User,Account,Transaction,Budget,TransactionCategory,CreditCardDetails,CreditCardStatement],
+});
